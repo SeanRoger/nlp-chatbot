a976c531b145372f33e96a71bc73af067090dfc9
SeanRoger <147505099+SeanRoger@users.noreply.github.com>
2023-10-14 07:34:55 +0000

    modified:   Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -6,11 +6,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
 
-# Initialize 'messages' in session_state if not present
+# Initialize 'messages' and 'last_active_time' in session_state if not present
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 
-url = 'chatbot_dataset.csv'
+if 'last_active_time' not in st.session_state:
+    st.session_state.last_active_time = time.time()
+
+# Load data from URL
+url = 'https://raw.githubusercontent.com/SeanRoger/nlp-chatbot/main/chatbot_dataset.csv'
 
 try:
     response = requests.get(url)
@@ -34,13 +38,21 @@ else:
         elif message["role"] == "Megiddo Bot":
             st.text(f"{bot_icon} Megiddo Bot:")
             st.text_area("Megiddo Bot", value=message["content"], height=100)
+            st.text_area("Category", value=message["category"], height=100)
 
-    if prompt := st.text_input("User Input"):
-        st.session_state.messages.append({"role": "User", "content": prompt})
-        
+    if time.time() - st.session_state.last_active_time > 10:
+        st.text("Please type your next question.")
+
+    user_input = st.text_input("User Input", key="user_input")
+    if user_input:
+        st.session_state.last_active_time = time.time()
+        st.session_state.messages = []
+
+        st.session_state.messages.append({"role": "User", "content": user_input})
+
         try:
             vectorizer = TfidfVectorizer()
-            all_data = list(df['Query']) + [prompt]
+            all_data = list(df['Query']) + [user_input]
             tfidf_matrix = vectorizer.fit_transform(all_data)
             document_vectors = tfidf_matrix[:-1]
             query_vector = tfidf_matrix[-1]
@@ -48,14 +60,8 @@ else:
             sorted_indexes = similarity_scores.argsort()[0][-1:]
             response = df.iloc[sorted_indexes[0]]['Response']
             category = df.iloc[sorted_indexes[0]]['Category']
-            
-            st.text(f"{bot_icon} Megiddo Bot:")
-            st.text_area("Megiddo Bot", value=response, height=100)
-            st.text_area("Category", value=category, height=100)
-            st.session_state.messages.append({"role": "Megiddo Bot", "content": response})
 
-            # Introduce a delay of 10 seconds before prompting the user for the next question
-            time.sleep(10)
-            st.text("Please type your next question.")
+            st.session_state.messages.append({"role": "Megiddo Bot", "content": response, "category": category})
+
         except KeyError:
             st.error("Invalid data format in the CSV file. Please check the format and try again.")
